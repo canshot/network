027bb99cdbe498205bb21844cf6074c6c8b72ce4
Jaewan Lim <31329026+canshot@users.noreply.github.com>
2018-08-28 14:48:03 -0400

    Update 0828.py

--- 0828.py
+++ 0828.py
@@ -13,9 +13,12 @@ g.add_edge(1,1)
 
 #1. The number of nodes in the network
 g.number_of_nodes()
+#outcome = 3
 
 #2. The number of self-edge in the network
 g.number_of_selfloops()
+#outcome = 1
 
 #3. The number of directed edges in the network
 g.number_of_edges()
+#outcome = 4
